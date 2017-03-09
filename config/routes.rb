574d8e42cb15e17e20a9fed39c6e2959195bc35b
cordwartman <cordwartman@gmail.com>
2017-03-09 14:20:07 -0500

    added a users controller with show action to display profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
+  
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  root 'users#show'
 end
